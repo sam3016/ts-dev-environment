b7c41442abd1e14ec21a9e77ccfbc3058350c0ab
Sam.H <samhui3016@gmail.com>
2022-10-22 00:25:21 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+//Here we're importing items we'll need. You can add other imports here.
+//The first function. Remove this.
+const btn = document.querySelector("button");
+if (btn) {
+    btn.onclick = function () {
+        alert("You ran some TypeScript");
+    };
+}
+//
